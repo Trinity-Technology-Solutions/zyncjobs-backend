078e86a72a0124d9088cdb19682f9730aeaf9ac5
sureshbabu <suresh.image@gmail.com>
2026-03-05 23:05:43 +0530

    Server config fixes

--- server.js
+++ server.js
@@ -92,11 +92,6 @@ const io = new Server(httpServer, {
   cors: {
     origin: [
       'http://localhost:5173',
-      'https://trinity-jobs.vercel.app',
-      'https://trinity-jobs-ezblun328-mutheeswarans-projects.vercel.app',
-      'https://stagging.zyncjobs.com',
-      'https://zyncjobs.com',
-      'https://www.zyncjobs.com',
       process.env.FRONTEND_URL
     ].filter(Boolean),
     credentials: true
