8a9bd908d51fd6d0842a9505ec78c985bf62131e
muthees-trinity <muthees@trinitetech.com>
2026-04-17 14:23:58 +0530

    Delete fix_gdpr.sql

--- fix_gdpr.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE gdpr_consents ALTER COLUMN "resumeStatus" DROP DEFAULT;
-ALTER TABLE gdpr_consents DROP CONSTRAINT IF EXISTS "gdpr_consents_resumeStatus_check";
-ALTER TABLE gdpr_consents ALTER COLUMN "resumeStatus" TYPE "public"."enum_gdpr_consents_resumeStatus" USING ("resumeStatus"::"public"."enum_gdpr_consents_resumeStatus");
-ALTER TABLE gdpr_consents ALTER COLUMN "resumeStatus" SET DEFAULT 'active'::"public"."enum_gdpr_consents_resumeStatus";
