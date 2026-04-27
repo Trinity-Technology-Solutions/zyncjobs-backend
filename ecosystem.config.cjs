a59bc150b48dd20d857725096d62f64262f367a4
Muthees <113660161+Muthees@users.noreply.github.com>
2026-04-27 12:09:23 +0530

    new

---
+++ ecosystem.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [{
+    name: 'zyncjobs-backend',
+    script: 'server.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development',
+      PORT: 5000
+    },
+    env_qa: {
+      NODE_ENV: 'qa',
+      PORT: 5000
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 5000
+    }
+  }]
+};
